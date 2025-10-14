13eae08444035da1d0444be32108034b955ee605
richardcai <aly.moby@gmail.com>
2025-10-14 19:54:28 -0400

    Removed Backend

--- server/Models/ProjectDto.cs
+++
@@ -1,3 +0,0 @@
-namespace Qiyuan.Api.Models;
-
-public record ProjectDto(int Id, string Title, string Description, string? Url, string? Repo);
--- server/Services/IProjectService.cs
+++
@@ -1,7 +0,0 @@
-using Qiyuan.Api.Models;
-namespace Qiyuan.Api.Services;
-
-public interface IProjectService
-{
-    IEnumerable<ProjectDto> GetAll();
-}
